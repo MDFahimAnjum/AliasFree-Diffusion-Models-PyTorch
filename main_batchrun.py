6c70ad40614fd31ca3b2f9f139d70ac331afde2e
mdfahim-anjum <mdfahim-anjum@uiowa.edu>
2024-08-24 08:34:06 +0000

    ready for MNIST local run

--- main_batchrun.py
+++ main_batchrun.py
@@ -13,24 +13,24 @@ datapath = os.path.join(current_directory,"data\MNIST\mnist_train_small.csv")
 
 params={
     'unet_v': 0,
-    'epochs': 2,
-    'batchsize': 32,
+    'epochs': 100,
+    'batchsize': 64,
     'image_size': 32,
     'image_channels': 1,
     'device': "cuda",
     'lr': 3e-4,
     'noise_steps': 1000,
     'image_gen_per_epoch': 8,
-    'f_kernel': None,#3,
-    'f_beta': None,#8,
-    'f_down': None,#np.pi/4,
-    'f_up': None,#np .pi,
+    'f_kernel': None,
+    'f_beta': None,
+    'f_down': None,
+    'f_up': None,
     'save_trining': False,
-    'gen_per_batch': 25,#200,
-    'gen_total': 50,#3000,
+    'gen_per_batch': 250,
+    'gen_total': 2000,
     'seed': 42,
-    'collage_n_per_image': 25,#400,
-    'collage_n': 50,#2000,
+    'collage_n_per_image': 400,
+    'collage_n': 2000,
     'dataset': "MNIST",
     'dataset_dir':  datapath
 }
@@ -38,9 +38,9 @@ params={
 ddpm_run(params)
 
 params['f_kernel']=3
-params['f_beta']=8
+params['f_beta']=2
 params['f_down']=np.pi/2
-params['f_up']=np.pi
+params['f_up']=np.pi/2
 
 unet_v_all=[1,2,3]
 
@@ -48,6 +48,7 @@ for unet_v in unet_v_all:
     params['unet_v']=unet_v
     ddpm_run(params)
 
+"""
 # MNIST-M
 #---------
 datapath = os.path.join(current_directory,"data\MNIST-M-6000")
@@ -89,5 +90,4 @@ for unet_v in unet_v_all:
     params['unet_v']=unet_v
     ddpm_run(params)
 
-
-# %%
+"""
